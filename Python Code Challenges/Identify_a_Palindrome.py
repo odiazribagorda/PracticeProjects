486e3d8a170c67c4f8df89fc205524ad37b7a824
Oscar <oscdiaz@ucm.es>
2020-10-28 19:59:29 +0100

    More challenges done

--- Python Code Challenges/Identify_a_Palindrome.py
+++ Python Code Challenges/Identify_a_Palindrome.py
@@ -1,5 +1,9 @@
-def is_palindrome(word):
+def is_palindrome(w):
     ## We clean the string
-    word.lower()
-    word = ''.join(filter(lambda x: x.isalpha(),word))
-    
+    w.lower()
+    w = ''.join(filter(lambda x: x.isalpha(),w))
+    ## The string is reduced while start and end are equal.
+    while(len(w) > 1 and w[0]==w[len(w)-1]):
+        w=w[1:len(w)-1]
+    ## Returns if the processed was finished with at most one letter.
+    return len(w) <= 1
---
+++ Python Code Challenges/sort_a_string.py
@@ -0,0 +1,12 @@
+def sort_words(input):
+    l = input.split()
+    ## Bubble sorting the words
+    for i in range(1,len(l)):
+        aux = l[i]
+        j = i-1
+        while(j >= 0 and aux.lower()<l[j].lower()):
+            l[j+1]=l[j]
+            j = j-1
+        l[j+1]=aux
+    return ' '.join(l)
+        
